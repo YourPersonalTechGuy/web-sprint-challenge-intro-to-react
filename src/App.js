acbc9588552a005b17422f881252fa51c3433158
Anas Abdelsalam <anas.abdelsalam57@gmail.com>
2021-02-19 16:44:08 -0600

    completed sprint

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Axios from 'axios';
 import './App.css';
+import './components/Character'
+import axios from 'axios';
+import Character from './components/Character';
+import styled from "styled-components";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +13,76 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const baseURL = "https://swapi.dev/api/"
+  const [total, setTotal] = useState(0)
+  const [people, setPeople] = useState([])
+  const [id, setID] = useState(0)
+  
 
+
+useEffect(() => {
+   return total <= 82 ? creatingPeople(total+1) : console.log(people)
+},[total])
+
+const creatingPeople = (personIndex) => {
+ axios.get(`${baseURL}people/${personIndex}/`)
+ .then((res) => {
+  let persons = people;
+  console.log(res.data)
+  persons.push(res.data)
+  setPeople(persons)
+   
+ })
+ .catch((err) => {
+   console.log(err)
+   setTotal(total+1)
+  })
+ .then(() => {
+  setTotal(total+1)
+ })
+}
+  const StA = styled.a`
+    font-weight: bold;
+    display: block;
+    padding: 5%;
+    width: 100%;
+    height: 100%;
+    background-color:  rgb(0 ,238 ,255 ,.5);
+  &:hover{
+    cursor: pointer;
+    background-color: rgb(36, 226, 36,.5);
+  }
+  `
+  const StLi = styled.li`
+    list-style: none;
+    text-align: left;
+    margin:0%;
+    text-align: center;
+  `
+  const LiDiv = styled.div`
+    display: inline;
+    width: fit-content;
+    height: 80.25%;
+    padding: 0%;
+  `
+
+const creatingList = people.map((person,index)=>{
+  return (
+  <StLi>
+      <StA index={index} onClick={() => {setID(index)}}>{person.name}</StA>
+  </StLi>)
+})
+
+
+  
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Characters viewer</h1>
+        {people.length < 82 && <span>{`loading page ${total}/82`}</span>}
+        <LiDiv>
+          {people.length < 82 ? null : <ul>{creatingList}</ul>}
+        </LiDiv>
+        {people.length < 82 ? null : <Character people={people} id={id} />} 
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,55 @@
+import React, {useState, useEffect} from 'react';
+import styled from 'styled-components';
+
 // Write your Character component here
+
+const Character = (props) => {
+
+    const {people, id} = props;
+
+    const [name, setName] = useState("")
+    const [height, setHeight] = useState("")
+    const [mass, setMass] = useState("")
+    const [hairColor, setHairColor] = useState("")
+    const [skinColor, setSkinColor] = useState("")
+    const [eyeColor, setEyeColor] = useState("")
+    const [birthYear, setBirthYear] = useState("")
+    const [gender, setGender] = useState("")
+    const [films, setFilms] = useState([])
+
+    useEffect(() => {
+        setName(people[id].name);
+        setHeight(people[id].height);
+        setMass(people[id].mass);
+        setHairColor(people[id].hair_color);
+        setSkinColor(people[id].skin_color)
+        setEyeColor(people[id].eye_color);
+        setBirthYear(people[id].birth_year);
+        setGender(people[id].gender);
+        // setFilms(people[id].films)
+    },[id])
+
+
+    const CharDiv =styled.div`
+        display: flex column;
+        margin-top: -52%;
+        height: 100%;
+        justify-content: space-evenly;
+    `
+
+
+    return (
+        <CharDiv>
+            <h2>{name}</h2>
+            <p>{`Birth Year: ${birthYear}`}</p>
+            <p>{`Gender: ${gender}`}</p>
+            <p>{`Height: ${height} cm`}</p>
+            <p>{`Mass: ${mass} kg`}</p>
+            <p>{`Hair Color: ${hairColor}`}</p>
+            <p>{`Skin Color: ${skinColor}`}</p>
+            <p>{`Eye Color: ${eyeColor}`}</p>
+        </CharDiv>
+    )
+}
+
+export default Character;
